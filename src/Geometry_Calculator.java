fc5261c0815eee51eaca279111ae6b478834fb3c
genesissssssss <bautistamarcgenesis@gmail.com>
2026-08-24 01:09:04 +0800

    Geometry Calculator1

--- src/Geometry_Calculator.java
+++ src/Geometry_Calculator.java
@@ -4,27 +4,27 @@ public class Geometry_Calculator {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the side leght of the square: ");
+        System.out.print("Enter the side length of the square: ");
         double side = scanner.nextDouble();
 
         double squareArea = side * side;
         double squarePerimeter = 4 * side;
 
         System.out.println("Square Area: " + squareArea);
-        System.out.println("Square Parameter: " +squarePerimeter);
+        System.out.println("Square Perimeter: " +squarePerimeter);
         System.out.println();
 
-        System.out.print("Enter rectangle lenght: ");
+        System.out.print("Enter rectangle length: ");
         double length = scanner.nextDouble();
 
         System.out.print("Enter rectangle width: ");
         double width = scanner.nextDouble();
 
-        double area = length * length;
+        double area = length * width;
         double perimeter = 2 * (length + width);
 
         System.out.println("The rectangle area: " + area);
-        System.out.println("The rectangle parimeter:" + perimeter);
+        System.out.println("The rectangle perimeter:" + perimeter);
 
 
 
