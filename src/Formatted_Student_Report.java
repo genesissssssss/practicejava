7a5b017fb9a0a0efd4dc1009cbd086ed43f2f28e
genesissssssss <bautistamarcgenesis@gmail.com>
2026-09-22 01:59:15 +0800

    Formatted Student Report

---
+++ src/Formatted_Student_Report.java
@@ -0,0 +1,16 @@
+public class Formatted_Student_Report {
+    public static void main(String[] args){
+
+        String name = "Marc";
+        int age = 24;
+        double gpa = 2.25;
+        String status = "passed";
+
+        System.out.printf("Name    :%s\n", name);
+        System.out.printf("Age     :%d\n", age);
+        System.out.printf("GPA     :%f\n", gpa);
+        System.out.printf("Status  :%s\n" , status);
+
+    }
+
+}
